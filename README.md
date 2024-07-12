8f6d9c58bb2568a90040a8cc4708e334badcc6f9
Talha Ahmad <talha05ahmad@gmail.com>
2024-07-12 16:41:45 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### REMS Sensor Configuration Program
+This program is meant to add sensors, edit existing sensors, and view overall sensor data. At the moment, the way this program works is by editing the actual
+Arduino code. The supporting example.ino file is the one that's being edited by the program, but eventually, the goal is to use this in an actual production
+environment with it actually editing physical files on working Arduinos. 
+
+In regards to current development, the program can successuflly add sensor pins to the Arduino code, but there is a bug where it cannot add the sensor to
+the array or to the actual webpage. This is a work in progress and will be fixed soon. The eventual goal is also to migrate from a TUI environment to a
+flask web server that can be accessed from any device on the network. However, the TUI program provides the benefit of being lightweight.
---
+++ sensor-configuration.py
@@ -0,0 +1,147 @@
+# Import TUI library
+import pytermgui as ptg
+
+
+# Dictionary to store the output of the input fields from the TUI.
+OUTPUT = {}
+
+# List that stores all the lines in the file with all the information added.
+finalFile = []
+
+# Function that is called when the submit button is pressed.
+def submit(manager: ptg.WindowManager, window: ptg.Window) -> None:
+
+    # Get the values from the input fields and store them in the OUTPUT dictionary.
+    for widget in window:
+        if isinstance(widget, ptg.InputField):
+            OUTPUT[widget.prompt] = widget.value
+            continue
+
+    # Close the window.
+    manager.stop()
+
+    # Open the file and read all the lines.
+    file = open("REMS1.ino", "r")
+
+    # Loop through all the lines in the file.
+    while True:
+
+        # Read the line.
+        line = file.readline()
+
+        # Add the line to the finalFile list.
+        finalFile.append(line)
+
+        # Num to store the number of preexisting sensors
+        num = 0
+
+        # If the line contains "SENSOR PIN" then add the new sensor's pin to the file.
+        if "SENSOR PIN" in line:
+
+            # Loop through the file until we get an empty line.
+            while True:
+
+                # Read the line.
+                line = file.readline()
+
+                # If the line contains the sensor name then increment the num variable.
+                if OUTPUT["Sensor:"] in line:
+                    num += 1
+
+                # If the line is empty then add the new sensor to the file.
+                if line == "\n":
+                    line = "#define " + OUTPUT["Sensor:"] + str(num) + " " + OUTPUT["Pin:"] + "\n"
+                    finalFile.append(line)
+
+                    # Add the empty line to the file.
+                    finalFile.append("\n")
+                    break
+
+                # Add the existing sensor lines to the file.
+                finalFile.append(line)
+
+        # If SENSOR ARRAY is on the line then we need to update the sensor's array with the number of sensors.
+        if "SENSOR ARRAY" in line:
+
+            # Loop through the file until we get the sensor's array.
+            while True:
+
+                # Read the line.
+                line = file.readline()
+
+                # If the line contains the sensor name then update the array with the new number of sensors.
+                if OUTPUT["Sensor:"] in line:
+                    line = "float " + OUTPUT["Sensor:"] + "[" + str(num + 1) + "]" + " = {0};\n"
+                    finalFile.append(line)
+                    break
+
+                # Add the existing sensor lines to the file.
+                finalFile.append(line)
+
+        if "SENSOR WEBPAGE" in line:
+            while True:
+                line = file.readline()
+
+                if line == "\n":
+                    line = "client.println(\"" + OUTPUT["Sensor:"] + "\");\n"
+                    finalFile.append(line)
+                    finalFile.append("\n")
+                    break;
+                
+                finalFile.append(line)
+
+
+        # If we reach the end of the file then break the loop.
+        if line == "":
+            break
+
+    file.close()
+    file = open("REMS1.ino", "w")
+    file.writelines(finalFile)
+
+
+        
+
+# STUFF TO GET INPUT. TEMPORARY
+CONFIG = """
+config:
+    InputField:
+        styles:
+            prompt: dim italic
+            cursor: '@72'
+    Label:
+        styles:
+            value: dim bold
+
+    Window:
+        styles:
+            border: '60'
+            corner: '60'
+
+    Container:
+        styles:
+            border: '96'
+            corner: '96'
+"""
+
+with ptg.YamlLoader() as loader:
+    loader.load(CONFIG)
+
+with ptg.WindowManager() as manager:
+    window = (
+        ptg.Window(
+            "",
+            ptg.InputField(" \"REMS1\" or \"REMS2\"", prompt="REMS Version:"),
+            ptg.InputField(" \"DHT11\" or \"DS18B20\"", prompt="Sensor:"),
+            ptg.InputField(" Pin number for the sensor", prompt="Pin:"),
+            "",
+            "",
+            ["Submit", lambda *_: submit(manager, window)],
+            width=60,
+            box="DOUBLE",
+        )
+        .set_title("[210 bold]New Sensor")
+        .center()
+    )
+
+    manager.add(window)
